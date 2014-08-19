a7803a5a47002fb16f7702b629577cda5590ae93
Paul Tagliamonte <tag@pault.ag>
2014-08-19 15:58:32 -0400

    more helpstrings

--- opencivicdata/api/client.py
+++ opencivicdata/api/client.py
@@ -16,6 +16,9 @@ class OCDAPI(Service):
 
     def _get(self, entity_id, **kwargs):
         """
+        Get an Object in the Open Civic Data API by it's ID, which
+        is a valid API path from the root of the app. This method
+        is the means through which all other methods hit the API.
         """
         return self._query(
             "GET",
@@ -26,6 +29,11 @@ class OCDAPI(Service):
 
     def organizations(self, **kwargs):
         """
+        Get all organizations, with params **kwargs.
+
+        All fields will be used to filter on, with the exception
+        of the special ``fields`` param, which will limit the
+        response fields.
         """
         return self._query(
             "GET",
@@ -36,25 +44,55 @@ class OCDAPI(Service):
 
     def organization(self, organization_id, *args, **kwargs):
         """
+        Get an organization ``organization_id`` from the API,
+        filtering on ``**kwargs``.
+
+        There is no validation done on the object ID. If the object
+        ID is of a different type, this method will return that
+        object without validating.
         """
         return self._get(organization_id, *args, **kwargs)
 
     def person(self, person_id, *args, **kwargs):
         """
+        Get a person ``person_id`` from the API, filtering on
+        ``**kwargs``.
+
+        There is no validation done on the object ID. If the object
+        ID is of a different type, this method will return that
+        object without validating.
         """
         return self._get(person_id, *args, **kwargs)
 
     def bill(self, bill_id, *args, **kwargs):
         """
+        Get a bill ``bill_id`` from the API, filtering on
+        ``**kwargs``.
+
+        There is no validation done on the object ID. If the object
+        ID is of a different type, this method will return that
+        object without validating.
         """
         return self._get(bill_id, *args, **kwargs)
 
     def vote(self, vote_id, *args, **kwargs):
         """
+        Get a vote ``vote_id`` from the API,
+        filtering on ``**kwargs``.
+
+        There is no validation done on the object ID. If the object
+        ID is of a different type, this method will return that
+        object without validating.
         """
         return self._get(vote_id, *args, **kwargs)
 
     def event(self, event_id, *args, **kwargs):
         """
+        Get an event ``organization_id`` from the API,
+        filtering on ``**kwargs``.
+
+        There is no validation done on the object ID. If the object
+        ID is of a different type, this method will return that
+        object without validating.
         """
         return self._get(event_id, *args, **kwargs)
