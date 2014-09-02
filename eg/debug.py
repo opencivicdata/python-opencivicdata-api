35e6e7e45eb988e1c5750ce0f59a0597ad6e9ce3
Paul Tagliamonte <tag@pault.ag>
2014-09-02 14:26:19 -0400

    fiddle with debugging

--- eg/debug.py
+++ eg/debug.py
@@ -1,6 +1,17 @@
 from opencivicdata.api import OCDAPI
+import statistics
+
 
 api = OCDAPI(host="http://localhost:8000")
 
-r = api.people()
-r.print_debug()
+response = api.people()
+times = []
+while response:
+    times.append(response.debug['time']['seconds'])
+    response = response.next()
+
+
+print("Total: {}".format(len(times)))
+print("Min:   {}".format(min(times)))
+print("Max:   {}".format(max(times)))
+print("Mean:  {}".format(statistics.mean(times)))
--- eg/test.py
+++
@@ -1,8 +0,0 @@
-from opencivicdata.api import OCDAPI
-
-api = OCDAPI(host="http://localhost:8000")
-
-response = api.people()
-while response:
-    print(response)
-    response = response.next()
